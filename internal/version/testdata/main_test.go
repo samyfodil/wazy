b0b389da98071c79902598e0bb28ad3acc8bb8b5
samyfodil <fodil.samy@gmail.com>
2026-07-13 02:29:52 -0500

    fix: point internal/version/testdata module at wazy, not wazero
    The nested version/testdata module (run by 'make test') still declared
    module tetratelabs/wazero and called version.GetWazeroVersion. Repoint its
    go.mod/imports to samyfodil/wazy and use GetWazyVersion. Fixes 'make test'
    on all platforms.

--- internal/version/testdata/main_test.go
+++ internal/version/testdata/main_test.go
@@ -3,8 +3,8 @@ package main
 import (
 	"testing"
 
-	"github.com/tetratelabs/wazero/internal/testing/require"
-	"github.com/tetratelabs/wazero/internal/version"
+	"github.com/samyfodil/wazy/internal/testing/require"
+	"github.com/samyfodil/wazy/internal/version"
 )
 
 // TestGetWazeroVersion ensures that GetWazeroVersion returns the version of wazero in the go.mod in the
@@ -12,5 +12,5 @@ import (
 func TestGetWazeroVersion(t *testing.T) {
 	// This matches the one in the "replace" statement in the go.mod.
 	const exp = "v0.0.0-20220818123113-1948909ec0b1"
-	require.Equal(t, exp, version.GetWazeroVersion())
+	require.Equal(t, exp, version.GetWazyVersion())
 }
